bcb2efec473f41b0635d14c34f8a7c01f6c57ffd
Paromita Roy <proy13@asu.edu>
2023-04-19 13:27:12 -0700

    Adding control keywords to the lexer

--- src/lexer.py
+++ src/lexer.py
@@ -4,10 +4,12 @@ import sys
 def lexer(filename):
     token_list = []
     reserved_keywords = ['int', 'string', 'bool', 'print']
+    control_keywords = ['for', 'while', 'if', 'then', 'else', 'range', 'in']
 
     token = ""
     with open(filename, 'r') as file:
         lines = file.readlines()
+        print(lines)
         for line in lines:
             for i in line:
                 if i == '\t':
@@ -23,11 +25,18 @@ def lexer(filename):
                     token_list.append(each_keyword)
                     each_keyword = ''
                     token[i:]
+                if each_keyword in control_keywords:
+                    if each_keyword + token[i:] == "int\n":
+                        each_keyword += token[i]
+                    token_list.append(each_keyword)
+                    each_keyword = ''
+                    token[i:]
                 if each_keyword not in reserved_keywords:
                     each_keyword += token[i]
                 else:
                     continue
             token = ''
+
 
     return token_list
 
