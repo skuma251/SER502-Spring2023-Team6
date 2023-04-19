c1be8a604934247aacb1b1266ea6b6214230d4d0
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2023-04-18 23:29:24 -0700

    Basic code for lexer and reserved_keywords

---
+++ src/lexer.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def lexer(filename):
+    token_list = []
+    reserved_keywords = ['int', 'string', 'bool', 'print']
+
+    token = ""
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            for i in line:
+                if i == '\t':
+                    continue
+                elif i == ' ':
+                    continue
+                else:
+                    token += i
+
+            each_keyword = ''
+            for i in range(len(token)):
+                if each_keyword in reserved_keywords:
+                    token_list.append(each_keyword)
+                    each_keyword = ''
+                    token[i:]
+                if each_keyword not in reserved_keywords:
+                    each_keyword += token[i]
+                else:
+                    continue
+            token = ''
+
+    return token_list
+
+
+if __name__ == "__main__":
+    fileName = sys.argv[1]
+    if fileName.endswith(".xyz"):
+        tokens = lexer(fileName)
+        print(tokens)
+    else:
+        print("Please pass file name with .xyz extension")
