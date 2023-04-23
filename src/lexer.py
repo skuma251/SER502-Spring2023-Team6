176e0a6c018ffdcc214297b97f526c39e58eadad
Paromita Roy <proy13@asu.edu>
2023-04-22 21:41:19 -0700

    Initial changes with inbuilt python lexer libraries

--- src/lexer.py
+++ src/lexer.py
@@ -1,50 +1,42 @@
 import sys
+import ply.lex as lex
 
+tokens = ('RES',
+            'INT',
+            'OPERATOR',
+            )
 
-def lexer(filename):
-    token_list = []
-    reserved_keywords = ['int', 'string', 'bool', 'print']
-    control_keywords = ['for', 'while', 'if', 'then', 'else', 'range', 'in']
+def t_RES(t):
+    r'int|string|bool|print'
+    return t
 
-    token = ""
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        print(lines)
-        for line in lines:
-            for i in line:
-                if i == '\t':
-                    continue
-                elif i == ' ':
-                    continue
-                else:
-                    token += i
+def t_INT(t):
+    r'\d+'
+    return t
 
-            each_keyword = ''
-            for i in range(len(token)):
-                if each_keyword in reserved_keywords:
-                    token_list.append(each_keyword)
-                    each_keyword = ''
-                    token[i:]
-                if each_keyword in control_keywords:
-                    if each_keyword + token[i:] == "int\n":
-                        each_keyword += token[i]
-                    token_list.append(each_keyword)
-                    each_keyword = ''
-                    token[i:]
-                if each_keyword not in reserved_keywords:
-                    each_keyword += token[i]
-                else:
-                    continue
-            token = ''
+def t_OPERATOR(t):
+    r'[-+*/%<>&|^~]|==|='
+    if t.value == '==':
+        t.type = 'OPERATOR'
+    return t
 
+t_ignore = ' \t\n'
 
-    return token_list
+def t_error(t):
+    raise TypeError("Unknown text '%s'" % (t.value,))
 
 
 if __name__ == "__main__":
     fileName = sys.argv[1]
     if fileName.endswith(".xyz"):
-        tokens = lexer(fileName)
-        print(tokens)
+        f = open(fileName, "r")
+        Lines = f.readlines() 
+        for line in Lines:
+            line = line.rstrip()
+            if line:
+                lexer = lex.lex()
+                lexer.input(line)
+                for tok in iter(lexer.token, None):
+                    print(tok.value)
     else:
         print("Please pass file name with .xyz extension")
