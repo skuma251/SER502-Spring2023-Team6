ca174b9584cb414aaf65e8532ac3485aaf8afc60
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2023-04-28 22:21:43 -0700

    Updating evaluator

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -30,6 +30,10 @@ eval_cmd(t_multicmd(T1),Env, NewEnv) :-
 eval_cmd(t_singlecmd(D), Env, NewEnv) :-
     eval_cmd1(D, Env, NewEnv).
 
+
+eval_cmd(T1,Env, NewEnv) :-
+    eval_cmd1(T1, Env, NewEnv).
+
 eval_cmd1(t_assign(t_ident(T), Exp), Env, NewEnv) :-
     eval_exp(Exp, Env, Val),
     update(T, Val, Env, NewEnv).
@@ -63,6 +67,9 @@ forrange(Id, From, To, Cmds, Env, NewEnv) :-
     Next is From + 1,
     forrange(Id, Next, To, Cmds, Env2, NewEnv).
 
+eval_cmd1(t_ternary(T1,T2,_),Env,NewEnv) :- bool_eval(T1,Env,Env1,true),eval_cmd(T2,Env1,NewEnv).
+eval_cmd1(t_ternary(T1,_,T2),Env,NewEnv):- bool_eval(T1,Env,Env1,false),eval_cmd(T2,Env1,NewEnv).
+
 
 bool_eval(t_bool_true(true), _, _, 'true').
 bool_eval(t_bool_false(false), _, _, 'false').
@@ -83,6 +90,17 @@ bool_eval(t_bool_greater_than(Exp1, Exp2), Env, _, 'false') :-
     eval_exp(Exp1, Env, Val1),
     eval_exp(Exp2, Env, Val2),
     Val1 < Val2.
+
+bool_eval(t_bool_equal(T1, T2), Env, _, 'true') :-
+    eval_exp(T1, Env, Val1),
+    eval_exp(T2, Env, Val2),
+    Val1 == Val2.
+
+bool_eval(t_bool_equal(T1, T2), Env, _, 'false') :-
+    eval_exp(T1, Env, Val1),
+    eval_exp(T2, Env, Val2),
+    Val1 =\= Val2.
+
 
 eval_exp(t_exp(E), Env, Val) :-
     eval_expr(E, Env, Val).
@@ -108,6 +126,8 @@ eval_expr(t_paren(E), Env, Val) :-
 eval_expr(t_ident(Id), Env, Val) :-
     lookup(Id, Env, Val).
 eval_expr(t_num(Num), _, Num).
+
+
 
 %lookup function
 lookup(Var,[(Var,Val)|_],Val).
