7bbdd65895c6a264ec54ef658765163c206cbdcd
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2023-04-28 17:04:53 -0700

    Updating the evaluator

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -1,26 +1,71 @@
-program_eval(t_prog(P),NewEnv):- block_eval(P, [], NewEnv).
+eval_program(t_prog(P), NewEnv) :-
+    eval_block(P, [], NewEnv).
 
-%evaluation for block
-block_eval(t_block(T1), Env, Env1) :- dec_eval(T1,Env, Env1).
-block_eval(t_block(T1,T2),Env, NewEnv) :- dec_eval(T1,Env, Env1), block_eval(T2,Env1,NewEnv).
-block_eval(t_block(T1,T2),Env, NewEnv) :- exp_eval(T1, T2, Env, NewEnv).
-    
-dec_eval(t_dec(Declaration), Env, NewEnv) :- dec_eval1(Declaration, Env, NewEnv).
-dec_eval(t_dec(T1,T2),Env,NewEnv) :- dec_eval1(T1, Env, Env1), exp_eval(T1,T2, Env1,NewEnv).
+eval_block(t_block(T1), Env, NewEnv) :-
+    eval_dec(T1, Env, NewEnv).
+eval_block(t_block(T1,T2), Env, NewEnv) :-
+    eval_dec(T1, Env, Env1),eval_cmd(T2, Env1, NewEnv).
 
-dec_eval1(t_int(t_ident(Name)), Env, NewEnv) :- update(Name, Env, 0, NewEnv).
-dec_eval1(t_bool(t_ident(Name)), Env, NewEnv) :- update(Name, Env, false, NewEnv).
-dec_eval1(t_string(t_ident(Name)), Env, NewEnv) :- update(Name, Env, "", NewEnv).
+eval_dec(t_multdec(T1, T2), Env, NewEnv) :-
+    eval_dec(T1, Env, Env1),
+    eval_dec(T2, Env1, NewEnv).
 
-exp_eval(t_int(t_ident(Name)),t_exp(t_num(Value)), Env, NewEnv) :- update(Name, Env, Value,NewEnv).
-exp_eval(t_dec(t_ident(Name)),t_exp(t_num(Value)), Env, NewEnv) :- update(Name, Env, Value,NewEnv).
+eval_dec(T,Env,NewEnv) :- eval_singledec(T, Env, NewEnv).
+
+eval_dec(t_singledec(D), Env, NewEnv) :-
+    eval_singledec(D, Env, NewEnv).
+
+eval_singledec(t_declr_var(_, t_ident(X), Exp), Env, NewEnv) :-
+    eval_exp(Exp, Env, Val),
+    update(X, Val, Env, NewEnv).
+
+eval_cmd(t_multicmd(T1,T2),Env, NewEnv) :-
+    eval_cmd1(T1, Env, Env1), eval_cmd(T2, Env1, NewEnv).
+
+eval_cmd(t_multicmd(T1,T2),Env, NewEnv) :-
+    eval_cmd1(T1, Env, Env1),eval_cmd1(T2,Env1,NewEnv).
+
+eval_cmd(t_singlecmd(D), Env, NewEnv) :-
+    eval_cmd1(D, Env, NewEnv).
+
+eval_cmd1(t_assign(t_ident(T), Exp), Env, NewEnv) :-
+    eval_exp(Exp, Env, Val),
+    update(T, Val, Env, NewEnv).
+
+eval_cmd1(empty_command(), Env, Env).
 
 
-%Lookup function, looking for the variable 
-lookup(Var,[(Var,Val)|_],Val). 
+eval_exp(t_exp(E), Env, Val) :-
+    eval_expr(E, Env, Val).
+
+eval_expr(t_add(E1, E2), Env, Val) :-
+    eval_expr(E1, Env, Val1),
+    eval_expr(E2, Env, Val2),
+    Val is Val1 + Val2.
+eval_expr(t_sub(E1, E2), Env, Val) :-
+    eval_expr(E1, Env, Val1),
+    eval_expr(E2, Env, Val2),
+    Val is Val1 - Val2.
+eval_expr(t_mul(E1, E2), Env, Val) :-
+    eval_expr(E1, Env, Val1),
+    eval_expr(E2, Env, Val2),
+    Val is Val1 * Val2.
+eval_expr(t_div(E1, E2), Env, Val) :-
+    eval_expr(E1, Env, Val1),
+    eval_expr(E2, Env, Val2),
+    Val is Val1 / Val2.
+eval_expr(t_paren(E), Env, Val) :-
+    eval_expr(E, Env, Val).
+eval_expr(t_ident(Id), Env, Val) :-
+    lookup(Id, Env, Val).
+eval_expr(t_num(Num), _, Num).
+
+%lookup function
+lookup(Var,[(Var,Val)|_],Val).
 lookup(Var,[_|Tail],Val):- lookup(Var,Tail,Val).
 
 %update function
-update(Var, [], NewVar, [(Var,NewVar)]).
-update(Var, [(Var,_)|Tail], NewVar, [(Var, NewVar)|Tail]).
-update(Var, [Head|Tail], NewVar, [Head|NewEnv]) :-Head \= (Var,_),update(Var, Tail, NewVar, NewEnv).
+update(Var,NewVar, [], [(Var,NewVar)]).
+update(Var, NewVar, [(Var,_)|Tail], [(Var, NewVar)|Tail]).
+update(Var, NewVar, [Head|Tail], [Head|NewEnv]) :-Head \= (Var,_),update(Var,NewVar, Tail, NewEnv).
+
